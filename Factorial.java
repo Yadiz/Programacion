aae14ca007d50f012de8a2202e763cb11f22f050
Yadiz <yady.rdz@gmail.com>
2015-10-27 12:35:11 -0600

    ok

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Factorial{
+  public static void main(String args[]){
+  Scanner valor=new Scanner(System.in);
+  int a=valor.nextInt();
+    int fact=1;
+    for (int i=a; i>=1; i--){
+      if (i==1) {break;}
+      else {
+      fact=fact*i;
+    }
+  System.out.println("El factorial es: " + fact);
+  }
+}
+}
